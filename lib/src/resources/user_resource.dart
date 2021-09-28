3166da5cc2b7c07d3f529ddbeea9d3a6c4d9a40d
chrisvidal <chris@voilaah.com>
2021-09-28 14:39:08 +0800

    added user account registration

--- lib/src/resources/user_resource.dart
+++ lib/src/resources/user_resource.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: unnecessary_brace_in_string_interps
 
+import 'dart:convert';
+import 'dart:io';
+
 import 'resource.dart';
 import '../responses.dart';
 import '../client.dart';
@@ -11,5 +14,16 @@ class UserResource extends Resource {
     final data = {'userId': userId};
     final response = await client.get(host, ['api/${apiVersion}/users/${userId}']);
     return User.fromJson(response);
+  }
+
+  Future<Token> post(String email, String password, String confirm_password, {required String adminToken}) async {
+    dynamic extraHeaders = {
+      'Content-Type': 'application/json',
+      HttpHeaders.authorizationHeader: 'Bearer $adminToken',
+    };
+    final payload = {'Email': email, 'Password': password, 'ConfirmPassword': confirm_password};
+    final response = await client.post(host, ['api/${apiVersion}/accounts/register'],
+        data: json.encode(payload), extraHeaders: extraHeaders);
+    return Token.fromJson(response);
   }
 }
